71bc645a5cbcc85d9696846b157018cb79bf8690
Ernests Kalniņš <ernests.kalnins@tet.lv>
2021-07-06 08:56:28 +0300

    implement interface for each time unit

--- main.go
+++ main.go
@@ -6,57 +6,6 @@ import (
 	"time"
 )
 
-// period contains the time increments used in the table. It is used as a sum
-// type.
-type period struct {
-	years, months, days, hours int
-}
-
-// String returns a human readable string description of the period.
-func (p period) String() string {
-	if p.hours != 0 {
-		if p.hours > 0 {
-			return fmt.Sprintf("In %d hours", p.hours)
-		} else {
-			return fmt.Sprintf("%d hours ago", -p.hours)
-		}
-	}
-
-	if p.days != 0 {
-		if p.days > 0 {
-			return fmt.Sprintf("In %d days", p.days)
-		} else {
-			return fmt.Sprintf("%d days ago", -p.days)
-		}
-	}
-
-	if p.months != 0 {
-		if p.months > 0 {
-			return fmt.Sprintf("In %d months", p.months)
-		} else {
-			return fmt.Sprintf("%d months ago", -p.months)
-		}
-	}
-
-	if p.years != 0 {
-		if p.years > 0 {
-			return fmt.Sprintf("In %d years", p.years)
-		} else {
-			return fmt.Sprintf("%d years ago", -p.years)
-		}
-	}
-
-	return "Now"
-}
-
-func (p period) addTo(t time.Time) time.Time {
-	if p.hours != 0 {
-		return t.Add(time.Duration(p.hours) * time.Hour)
-	} else {
-		return t.AddDate(p.years, p.months, p.days)
-	}
-}
-
 func main() {
 	now := time.Now()
 
@@ -66,50 +15,117 @@ func main() {
 	}
 }
 
-var rows = []period{
-	{years: -10},
-	{years: -5},
-	{years: -3},
-	{years: -2},
-	{years: -1},
-	{months: -9},
-	{months: -6},
-	{months: -3},
-	{months: -2},
-	{months: -1},
-	{days: -28},
-	{days: -21},
-	{days: -14},
-	{days: -7},
-	{days: -3},
-	{days: -2},
-	{days: -1},
-	{hours: -12},
-	{hours: -6},
-	{hours: -3},
-	{hours: -2},
-	{hours: -1},
-	{},
-	{hours: 1},
-	{hours: 2},
-	{hours: 3},
-	{hours: 6},
-	{hours: 12},
-	{days: 1},
-	{days: 2},
-	{days: 3},
-	{days: 7},
-	{days: 14},
-	{days: 21},
-	{days: 28},
-	{months: 1},
-	{months: 2},
-	{months: 3},
-	{months: 6},
-	{months: 9},
-	{years: 1},
-	{years: 2},
-	{years: 3},
-	{years: 5},
-	{years: 10},
+type timeUnit interface {
+	fmt.Stringer
+	addTo(time.Time) time.Time
+}
+
+var rows = []timeUnit{
+	year(-10),
+	year(-5),
+	year(-3),
+	year(-2),
+	year(-1),
+	month(-9),
+	month(-6),
+	month(-3),
+	month(-2),
+	month(-1),
+	day(-28),
+	day(-21),
+	day(-14),
+	day(-7),
+	day(-3),
+	day(-2),
+	day(-1),
+	hour(-12),
+	hour(-6),
+	hour(-3),
+	hour(-2),
+	hour(-1),
+	now{},
+	hour(1),
+	hour(2),
+	hour(3),
+	hour(6),
+	hour(12),
+	day(1),
+	day(2),
+	day(3),
+	day(7),
+	day(14),
+	day(21),
+	day(28),
+	month(1),
+	month(2),
+	month(3),
+	month(6),
+	month(9),
+	year(1),
+	year(2),
+	year(3),
+	year(5),
+	year(10),
+}
+
+type year int
+
+func (y year) String() string {
+	if y > 0 {
+		return fmt.Sprintf("In %d years", y)
+	}
+	return fmt.Sprintf("%d years ago", -y)
+}
+
+func (y year) addTo(t time.Time) time.Time {
+	return t.AddDate(int(y), 0, 0)
+}
+
+type month int
+
+func (m month) String() string {
+	if m > 0 {
+		return fmt.Sprintf("In %d months", m)
+	}
+	return fmt.Sprintf("%d months ago", -m)
+}
+
+func (m month) addTo(t time.Time) time.Time {
+	return t.AddDate(0, int(m), 0)
+}
+
+type day int
+
+func (d day) String() string {
+	if d > 0 {
+		return fmt.Sprintf("In %d days", d)
+	}
+	return fmt.Sprintf("%d days ago", -d)
+}
+
+func (d day) addTo(t time.Time) time.Time {
+	return t.AddDate(0, 0, int(d))
+}
+
+type hour int
+
+func (h hour) String() string {
+	if h > 0 {
+		return fmt.Sprintf("In %d hours", h)
+	}
+	return fmt.Sprintf("%d hours ago", -h)
+}
+
+func (h hour) addTo(t time.Time) time.Time {
+	return t.Add(time.Duration(h) * time.Hour)
+}
+
+type now struct{}
+
+func (now) String() string {
+	return "Now"
+}
+
+func (now) addTo(t time.Time) time.Time {
+	return t
 }
